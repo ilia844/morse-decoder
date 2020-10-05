d8bd67eed5560c2f9c2b6844c11f05af121b1bbd
ilia844 <w5332660@mail.ru>
2020-10-05 15:41:42 +0300

    feat: complete task 'morse-decoder'

--- src/index.js
+++ src/index.js
@@ -38,7 +38,51 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    function getSymbolArr(expr) {
+        const SHIFT = 10;
+        let index = 0;  
+        let arr = [];
+
+        while (index < expr.length) {
+            arr.push(expr.substring(index, index + SHIFT));
+            index += SHIFT;
+        }
+        return arr;
+    }
+    
+    function trimZeros(str) {
+        while (str[0] === '0') {
+            str = str.slice(1);    
+        }
+        return str;
+    }
+
+    function getSymbol(str) {
+        let morse = '';
+        if (str === '**********') {
+            return ' ';
+        } else {
+            str = trimZeros(str);
+            let index = 0;
+            while (index < str.length) {
+                if (str.substring(index, index + 2) === '10') {
+                    morse += '.';
+                } else if (str.substring(index, index + 2) === '11') {
+                    morse += '-';
+                }
+                index += 2;
+            }
+            return MORSE_TABLE[morse];
+        }
+    }
+
+    let result = '';
+    let symbolArr = getSymbolArr(expr);
+    for (let item of symbolArr) {
+        result += getSymbol(item);
+    }
+    
+    return result;
 }
 
 module.exports = {
